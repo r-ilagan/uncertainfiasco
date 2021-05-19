822495d0363ad7ea1c82879231773799ec957843
Rach Ilagan <cachola.matthew@gmail.com>
2021-05-19 14:38:18 -0400

    Create raw functionality

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,33 @@
 import Head from 'next/head';
+import Link from 'next/link';
+
+import { blogPosts } from '../lib/data';
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Uncertain Fiasco</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <main>
         <h1>Uncertain Fiasco</h1>
       </main>
+
+      <div>
+        {blogPosts.map((item) => (
+          <div key={item.slug}>
+            <div>
+              <Link href={`/blog/${item.slug}`}>
+                <a>{item.title}</a>
+              </Link>
+            </div>
+            <div>{item.date}</div>
+            <div>{item.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
