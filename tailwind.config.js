af92452643dbba8a149f6c380d771880a92c65c1
Rach Ilagan <cachola.matthew@gmail.com>
2021-05-25 16:43:23 -0400

    Add two colours--seaweed and rose

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,28 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: {
+          light: '#F5EFE0',
+          DEFAULT: '#F0EAD6',
+          dark: '#272727',
+        },
+        rose: {
+          DEFAULT: '#C43156',
+          dark: '#A32947',
+          light: '#D24B6D',
+        },
+        seaweed: {
+          DEFAULT: '#177E89',
+          dark: '#084C61',
+          light: '#21B0C0',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
