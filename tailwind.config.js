69a269bc6a37696177fd0ab296e75c9b0d674002
Rach Ilagan <cachola.matthew@gmail.com>
2021-06-03 18:59:59 -0400

    feat: Add highlighter effect to links

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,8 +25,15 @@ module.exports = {
           css: {
             color: theme('colors.gray.800'),
             a: {
-              color: '#084C61',
-              '&:hover': { textDecoration: 'none' },
+              // color: '#084C61',
+              textDecoration: 'none',
+              margin: '0 -0.4em',
+              padding: '0.1em 0.4em',
+              borderRadius: '0.8em 0.3em',
+              background: 'transparent',
+              backgroundImage:
+                'linear-gradient(to right, rgba(2,0,36,0.1), rgba(33,176,192,0.7) 4%, rgba(35,192,209,0.3))',
+              boxDecorationBreak: 'clone',
             },
             img: {
               display: 'block',
