7c7e970ee8ae962b416026b0e4000ce95c077ddb
Rach Ilagan <cachola.matthew@gmail.com>
2021-06-23 18:17:08 -0400

    fix: update config to use webpack 5

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 module.exports = {
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty',
-      };
-    }
+  future: {
+    webpack5: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
 
     return config;
-  },
-  images: {
-    domains: ['images.unsplash.com'],
   },
 };
